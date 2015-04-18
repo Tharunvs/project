c1c37acbfc36c5e5a7a29c15072b3ecda2f9547b
TharunTez <tharuntez4u@gmail.com>
2015-04-18 02:45:28 -0400

    commit

---
+++ home.php
@@ -0,0 +1,39 @@
+
+		<div class="container">
+		    <div class="alert alert-success alert-dismissable" ng-show="success.length > 0">
+				<i class="fa fa-check"></i>
+				<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+				<b>Alert!</b>{{ success }}
+			</div>
+			<div class="row">
+				<div class="col-md-6" ng-repeat="data in categories">
+					<div class="elect_prodct">
+						<div class="hm_prod_hed">
+							<div class="hm_prod_text">{{ data.category_title }}</div>
+							<div class="hm_prod_ero"></div>					
+						</div>
+						<div class="hm_prod_box" ng-init="product = getProductData(data.category_id)">
+					        
+							<div class="col-md-6" ng-repeat="dc in product.content">
+	                            
+								<div class="hm-product">
+									<div class="hm-product_img">
+										<img ng-src="admin/upload/{{mySplit(dc.product_image,0)}}">
+									</div>
+									<div class="hm-product_dtel">
+										<p>{{ dc.product_title }}</p>
+										<h3>${{ dc.product_price }}</h3>
+									</div>
+								</div>
+							</div>
+							
+							
+							
+						</div>
+					</div>
+				</div>
+
+			</div>
+			
+		</div>
+		
