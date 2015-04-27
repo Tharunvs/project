f731147005ac17c248ccf20422cbbddf5673d28a
TharunTez <tharuntez4u@gmail.com>
2015-04-27 07:14:33 -0400

    commit

--- include/config.php
+++ include/config.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = '';
+$DB_USER = 'raviprolu';
+$DB_PASS = 'newpaltz12561';
 $DB_NAME = 'database';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
