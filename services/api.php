d13a2474f213b35b0361b0fc40061bbfe85abe50
TharunTez <tharuntez4u@gmail.com>
2015-05-08 20:18:24 -0400

    commit

--- services/api.php
+++ services/api.php
@@ -257,7 +257,8 @@
 			    $error = array('status' => "Error", "msg" => "Product Already In Your Wishlist.");
 				$this->response($this->json($error),200);
 			}else{
-			$query="INSERT INTO tbl_wishlist(product_id, user_id, status, datetime) VALUES ('".$id."','".$_SESSION['userid']."','Active','".date('Y-m-d H:i:s')."')";
+			//$query="INSERT INTO tbl_wishlist(product_id, user_id, status, datetime) VALUES ('".$id."','".$_SESSION['userid']."','Active','".date('Y-m-d H:i:s')."')";
+	        $query="INSERT INTO tbl_wishlist(product_id, user_id, status, datetime) VALUES ('".$id."','".$_SESSION['userid']."','Active','2015-06-07 12:06:45')";
 	
 			
 			$r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
@@ -280,7 +281,8 @@
 			    $error = array('status' => "Error", "msg" => "Product Already In Your Cart.");
 				$this->response($this->json($error),200);
 			}else{
-			$query="INSERT INTO tbl_cart(product_id, user_id, quantity, status, datetime) VALUES ('".$id."','".$_SESSION['userid']."','1','Active','".date('Y-m-d H:i:s')."')";
+			//$query="INSERT INTO tbl_cart(product_id, user_id, quantity, status, datetime) VALUES ('".$id."','".$_SESSION['userid']."','1','Active','".date('Y-m-d H:i:s')."')";
+	        $query="INSERT INTO tbl_cart(product_id, user_id, quantity, status, datetime) VALUES ('".$id."','".$_SESSION['userid']."','1','Active','2015-05-07 12:05:44')";
 	
 			
 			$r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
@@ -462,6 +464,187 @@
 			$this->response('',204);	// If no records "No Content" status
 		}
 		
+		private function placeOrder(){
+			if($this->get_request_method() != "POST"){
+				$this->response('',406);
+			}
+
+			$order = json_decode(file_get_contents("php://input"),true);
+			
+			$sql = "select tbl_cart.quantity,tbl_product.product_id,tbl_product.product_title,tbl_product.product_brand,tbl_product.product_price from tbl_cart join tbl_product on tbl_cart.product_id = tbl_product.product_id where user_id = '".$order['user_id']."'";
+			$r = $this->mysqli->query($sql) or die($this->mysqli->error.__LINE__);
+
+			if($r->num_rows > 0){
+				$result = array();
+				while($row = $r->fetch_assoc()){
+					$result[] = $row;
+				}
+				
+				$product = $this->json($result); // send user details
+			}
+			$sql2 = "select SUM(tbl_cart.quantity * tbl_product.product_price) AS val from tbl_cart join tbl_product on tbl_cart.product_id = tbl_product.product_id where user_id = '".$order['user_id']."'";
+			$r1 = $this->mysqli->query($sql2) or die($this->mysqli->error.__LINE__);
+            $row1 = $r1->fetch_assoc();
+			$amount = $row1['val'];
+			$order_id = time();
+		    //$date = date('Y-m-d');
+			$date = '2015-05-06';
+			$expiry = $order['end_month'].'-'.$order['end_year'];
+			
+			$query="INSERT INTO tbl_order(order_id, order_product, user_id, order_name, order_address, order_city, order_country, card_expiry, card_number, card_name, card_cvv, user_zipcode,order_amount,status,order_date) VALUES ('".$order_id."','".$product."','".$order['user_id']."','".$order['user_name']."','".$order['user_address']."','".$order['user_city']."','".$order['user_country']."','".$expiry."','".$order['card_no']."','".$order['card_name']."','".$order['cvv_no']."','".$order['user_zipcode']."','".$amount."','Pending','".$date."')";
+	
+			//$query = "INSERT INTO tbl_user(".trim($columns,',').") VALUES(".trim($values,',').")";
+			if(!empty($order)){
+				$insert_id = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+				if($insert_id > 0){
+				$sql2 = "delete from tbl_cart where user_id = '".$_SESSION['userid']."'";
+			    $this->mysqli->query($sql2) or die($this->mysqli->error.__LINE__);
+				foreach($result as $res){
+				   $sq = "UPDATE tbl_product SET product_quantity=(select (SUM(product_quantity) - '".$res['quantity']."') )  WHERE product_id='".$res['product_id']."'";
+				   $this->mysqli->query($sq) or die($this->mysqli->error.__LINE__);
+				}
+				$success = array('status' => "Success", "msg" => "Ordered Successfully.");
+				$this->response($this->json($success),200);
+				}else{
+				
+				}
+			}else
+				$this->response('',204);	//"No Content" status
+			}
+			
+		private function getmyorders(){
+			if($this->get_request_method() != "GET"){
+				$this->response('',406);
+			}
+            $id = $_SESSION['userid'];
+			$query = "select * from tbl_order where user_id = '".$_SESSION['userid']."'";
+			
+			$r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+
+			if($r->num_rows > 0){
+				$result = array();
+				while($row = $r->fetch_assoc()){
+					$result[] = $row;
+				}
+               
+				$this->response($this->json($result), 200); // send user details
+			}
+			$this->response('',204);
+			
+		   }
+		   
+		   private function productorders(){	
+			if($this->get_request_method() != "GET"){
+				$this->response('',406);
+			}
+			$id = (int)$this->_request['id'];
+			
+			$query="SELECT order_product from tbl_order where order_id = $id ";
+			$r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+    
+			if($r->num_rows > 0){
+				$result = $r->fetch_assoc();
+				
+				$this->response($result['order_product'], 200); // send user details
+			}
+			$this->response('',204);	// If no records "No Content" status
+		}
+		
+		
+		private function updaterating(){
+			if($this->get_request_method() != "GET"){
+				$this->response('',406);
+			}
+			
+			if(isset($_SESSION['userid'])){
+			$id = $_SESSION['userid'];
+			$rat = (int)$this->_request['id'];
+			$pro_id = (int)$this->_request['proid'];
+			$query="SELECT * from tbl_rating where product_id = $pro_id and user_id = $id ";
+			$r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+			if($r->num_rows > 0){
+				$update = "UPDATE tbl_rating SET rating = ".$rat." WHERE product_id=$pro_id and user_id = $id";
+				$this->mysqli->query($update) or die($this->mysqli->error.__LINE__);
+			}else{
+			    $insert = "INSERT INTO tbl_rating(product_id, user_id, rating) VALUES ('".$pro_id."','".$id."','".$rat."')";
+				$r = $this->mysqli->query($insert) or die($this->mysqli->error.__LINE__);
+			}
+			}
+			
+			$this->response('',204);	// "No Content" status
+		}
+		
+		private function getrating(){
+			if($this->get_request_method() != "GET"){
+				$this->response('',406);
+			}
+			
+			if(isset($_SESSION['userid'])){
+			$id = $_SESSION['userid'];
+			$pro_id = (int)$this->_request['id'];
+			$query="SELECT rating from tbl_rating where product_id = $pro_id and user_id = $id ";
+			$r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+			if($r->num_rows > 0){
+				$result = $r->fetch_assoc();
+				
+				$this->response($result['rating'], 200); // send user details
+			}
+			   $this->response(0, 200); // send user details
+
+			}
+			
+			$this->response('',204);	// "No Content" status
+		}
+		
+		private function givereview(){
+			if($this->get_request_method() != "GET"){
+				$this->response('',406);
+			}
+			
+			if(isset($_SESSION['userid'])){
+			$id = $_SESSION['userid'];
+			$review = $this->_request['review'];
+			
+			$pro_id = (int)$this->_request['proid'];
+			$query="SELECT * from tbl_rating where product_id = $pro_id and user_id = $id ";
+			$r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+			if($r->num_rows > 0){
+				$update = "UPDATE tbl_rating SET review = '".$review."' WHERE product_id=$pro_id and user_id = $id";
+				$this->mysqli->query($update) or die($this->mysqli->error.__LINE__);
+			    $success = array('status' => "Success", "msg" => "Review Updated Successfully.");
+				$this->response($this->json($success),200);
+			
+			}else{
+			    $insert = "INSERT INTO tbl_rating(product_id, user_id, review) VALUES ('".$pro_id."','".$id."','".$review."')";
+				$r = $this->mysqli->query($insert) or die($this->mysqli->error.__LINE__);
+			    $success = array('status' => "Success", "msg" => "Review Added Successfully.");
+				$this->response($this->json($success),200);
+			
+			}
+			}
+			
+			$this->response('',204);	// "No Content" status
+		}
+		
+		private function getreview(){	
+			if($this->get_request_method() != "GET"){
+				$this->response('',406);
+			}
+			
+			$query="SELECT tbl_rating.*,tbl_user.user_name,tbl_product.product_title from tbl_rating join tbl_user on tbl_rating.user_id=tbl_user.user_id join tbl_product on tbl_rating.product_id=tbl_product.product_id";
+			
+			$r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+
+			if($r->num_rows > 0){
+				$result = array();
+				while($row = $r->fetch_assoc()){
+					$result[] = $row;
+				}
+				
+				$this->response($this->json($result), 200); // send user details
+			}
+			$this->response('',204);	// If no records "No Content" status
+		}
 		
 		
 			private function changepass(){
